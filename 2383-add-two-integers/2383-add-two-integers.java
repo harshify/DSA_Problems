6f62463a677eb446ab7da6af907cc8fa0c74f8df
Harsh Raj Mishra <97730548+harshify@users.noreply.github.com>
2023-08-02 01:42:35 +0530

    Time: 0 ms (100%), Space: 39.5 MB (55.31%) - LeetHub

--- 2383-add-two-integers/2383-add-two-integers.java
+++ 2383-add-two-integers/2383-add-two-integers.java
@@ -1,6 +1,13 @@
 class Solution {
     public int sum(int num1, int num2) {
-       int sum= num1 + num2;
-       return sum;
+        int l = -200, r = 200;
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (mid == num1 + num2) { return mid; }
+            if (mid <  num1 + num2) l = mid + 1;
+            if (mid >  num1 + num2) r = mid - 1;
+        }
+        return l;
     }
 }
+        
