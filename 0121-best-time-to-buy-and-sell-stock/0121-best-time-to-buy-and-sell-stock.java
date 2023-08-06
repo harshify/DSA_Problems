cd8139804a98b511d2dec28243be943d14bd4712
Harsh Raj Mishra <97730548+harshify@users.noreply.github.com>
2023-08-06 21:15:01 +0530

    Time: 4 ms (6.13%), Space: 52.1 MB (99.54%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,41 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 1) {
-            return 0; // No profit can be obtained with fewer than 2 prices
+        int minSell = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i]<minSell) minSell = prices[i];
+            else maxProfit = (prices[i]-minSell < maxProfit)? maxProfit : prices[i]-minSell;
         }
-        
-        int minPrice = prices[0]; // Initialize the minimum price
-        int maxProfit = 0; // Initialize the maximum profit
-        
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < minPrice) {
-                minPrice = prices[i];
-            } else if (prices[i] - minPrice > maxProfit) {
-                maxProfit = prices[i] - minPrice;
-            }
-        }
-        
+        System.gc();
         return maxProfit;
     }
 }
-
-/*
-class Solution {
-    public int maxProfit(int[] prices) {
-        int lsf = Integer.MAX_VALUE;
-        int op = 0;
-        int pist = 0;
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){
-                lsf = prices[i];
-            }
-            pist = prices[i] - lsf;
-            if(op < pist){
-                op = pist;
-            }
-        }
-        return op;
-    }
-}
-*/
