9442a3d9a0e00870ad7710d68002cfb538a682f5
Harsh Raj Mishra <97730548+harshify@users.noreply.github.com>
2023-08-06 21:14:25 +0530

    Time: 2 ms (91.17%), Space: 61.1 MB (64.19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,27 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0; // No profit can be obtained with fewer than 2 prices
+        }
+        
+        int minPrice = prices[0]; // Initialize the minimum price
+        int maxProfit = 0; // Initialize the maximum profit
+        
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        
+        return maxProfit;
+    }
+}
+
+/*
+class Solution {
+    public int maxProfit(int[] prices) {
         int lsf = Integer.MAX_VALUE;
         int op = 0;
         int pist = 0;
@@ -16,3 +38,4 @@ class Solution {
         return op;
     }
 }
+*/
