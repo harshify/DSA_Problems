eb50803f7d33a93e0810ba7b97f396637ec9dcbc
Harsh Raj Mishra <97730548+harshify@users.noreply.github.com>
2023-08-13 22:26:22 +0530

    Time: 0 ms (100%), Space: 41 MB (66.08%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,4 +1,26 @@
+class Solution {
 
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) mid++;
+            else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+}
+
+/*
 class Solution {
     public void sortColors(int[] nums) {
         int count0 = 0;
@@ -16,3 +38,5 @@ class Solution {
 
     }
 }
+
+*/
